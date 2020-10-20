cadce601c17be5d44f968e4eabd5e5e420278d31
Junior Streichan <juniorstreichan.dev@gmail.com>
2020-10-20 15:51:25 -0300

    (JMS): create jms consumer

---
+++ jms-consumer/src/test/kotlin/test/jms/consumer/ConsumerApplicationTests.kt
@@ -0,0 +1,13 @@
+package test.jms.consumer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
